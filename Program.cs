a694f15b3f3cc61347666a4401b96600dbe04e19
tatyanka03 <63300782+tatyanka03@users.noreply.github.com>
2020-04-09 08:49:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace itProger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] digits = new byte[5];
+
+            digits[0] = 34;
+            digits[4] = 255;
+
+            Console.WriteLine(digits[3]);
+
+            string[] students = new string[] { "Bob", "Mickle", "George", "Alex" };
+
+            Console.WriteLine(students[1]);
+
+            short[,] nums = new short[5, 2];
+
+            int[,] numbers = {
+                  { 5, 7, 2 },
+                  { 9, 200, 8 },
+                  { 7, 0, 12 }
+             };
+            int len = students.Length;
+            Console.WriteLine(len);
+
+            //Array.Clear(students, 0, len);
+
+            Console.WriteLine(students[1]);
+
+            string[] copyStudents = new string[len];
+            students.CopyTo(copyStudents, 0);
+
+            Console.WriteLine(students[1]);
+
+
+            int[,,] numbers_2 = {
+            {
+             { 2, 5, 6 },
+             { 2, 5, 6 },
+             { 2, 5, 6 },
+            },
+             {
+                    { 2, 5, 6 },
+                    { 2, 5, 6 },
+                    { 2, 5, 6 }
+             },
+             };
+
+            numbers[2, 0] = 50;
+
+            Console.WriteLine(numbers[1, 1]);
+            
+            Console.ReadKey();
+         }
+    }
+}
